3844906a8c2d1493d85cc8763dc359a88dbfd68d
qrxtuv <max198920054@gmail.com>
2026-04-12 21:05:01 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://pastefy.app/qmhnulZB/raw"))()
+loadstring(game:HttpGet("https://jjgociynthbfobvfosol.supabase.co/functions/v1/get-script/1281dca5-f0b7-8d00-50c3-2d11d785c6e5.lua"))()
